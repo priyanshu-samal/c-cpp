2663e73797dd33b8d3a3976aed135c210a2d08e0
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-12-19 09:29:35 +0530

    movement

--- C/Ping Pong/main.c
+++ C/Ping Pong/main.c
@@ -19,25 +19,51 @@ int main(int argc, char *argv[])
 
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
-    SDL_Rect pl1 = (SDL_Rect){40, 40, 40, 150};
+    SDL_Rect pl1 = {40, 40, 40, 150};
+    SDL_Rect pl2 = {720, 40, 40, 150};
     Uint32 color = 0xFFFFFFFF;
+
     SDL_FillRect(surface, &pl1, color);
-
-    SDL_Rect pl2 = (SDL_Rect){720, 40, 40, 150};
     SDL_FillRect(surface, &pl2, color);
-
-
-
     SDL_UpdateWindowSurface(window);
 
-    SDL_Delay(3000);
-
     int running = 1;
+
     while (running) {
         SDL_Event event;
+
         while (SDL_PollEvent(&event)) {
+
             if (event.type == SDL_QUIT) {
                 running = 0;
+            }
+
+            if (event.type == SDL_KEYDOWN) {
+
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    running = 0;
+                }
+
+                if (event.key.keysym.sym == SDLK_w) {
+                    pl1.y -= 10;
+                }
+
+                if (event.key.keysym.sym == SDLK_s) {
+                    pl1.y += 10;
+                }
+
+                if (event.key.keysym.sym == SDLK_UP) {
+                    pl2.y -= 10;
+                }
+
+                if (event.key.keysym.sym == SDLK_DOWN) {
+                    pl2.y += 10;
+                }
+
+                SDL_FillRect(surface, NULL, 0x000000FF);
+                SDL_FillRect(surface, &pl1, color);
+                SDL_FillRect(surface, &pl2, color);
+                SDL_UpdateWindowSurface(window);
             }
         }
     }
