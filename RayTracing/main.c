15183fec076f0b7f8d512d37e3e2a8f0069e7458
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-12-22 10:58:56 +0530

    raytracing-3

--- RayTracing/main.c
+++ RayTracing/main.c
@@ -2,16 +2,29 @@
 #include <stdio.h>
 #include <math.h>
 
-#define WIDTH  1000
+#define WIDTH  1600
 #define HEIGHT 800
 
-#define COLOR_BLUE (SDL_Color){0, 0, 255, 255}
+#define COLOR_EARTH (SDL_Color){0, 0, 255, 255}
+#define COLOR_SUN   (SDL_Color){255, 200, 50, 255}
 
 typedef struct Circle {
     double x;
     double y;
     double radius;
 } Circle;
+
+
+typedef struct Shodow {
+    double x;
+    double y;
+    double radius;
+} Sun;
+
+void rays(Circle earth, Sun sun) {
+    
+
+}
 
 void FillCircle(SDL_Surface *surface, Circle circle, SDL_Color color) {
     Uint32 pixelColor = SDL_MapRGB(
@@ -21,13 +34,13 @@ void FillCircle(SDL_Surface *surface, Circle circle, SDL_Color color) {
     SDL_LockSurface(surface);
 
     Uint32 *pixels = (Uint32 *)surface->pixels;
-    int pitch = surface->pitch / 4; 
+    int pitch = surface->pitch / 4;
+
     for (int y = 0; y < surface->h; y++) {
         for (int x = 0; x < surface->w; x++) {
 
             double dx = x - circle.x;
             double dy = y - circle.y;
-
             double distance = sqrt(dx * dx + dy * dy);
 
             if (distance <= circle.radius) {
@@ -47,7 +60,7 @@ int main(int argc, char *argv[]) {
     }
 
     SDL_Window *window = SDL_CreateWindow(
-        "Circle using math.h",
+        "Sun & Earth (Original Mouse Logic)",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WIDTH,
@@ -69,21 +82,10 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-   
-    SDL_FillRect(
-        surface,
-        NULL,
-        SDL_MapRGB(surface->format, 0, 0, 0)
-    );
-
-   
-    Circle circle = {200, 200, 80};
-
     
-    FillCircle(surface, circle, COLOR_BLUE);
-    SDL_UpdateWindowSurface(window);
+    Circle earth = {200, 200, 80};
+    Sun sun      = {500, 400, 140};
 
-    
     int running = 1;
     SDL_Event event;
 
@@ -91,7 +93,36 @@ int main(int argc, char *argv[]) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
                 running = 0;
+
+            if (event.type == SDL_KEYDOWN &&
+                event.key.keysym.sym == SDLK_ESCAPE)
+                running = 0;
+
+            
+            if (event.type == SDL_MOUSEMOTION && event.motion.state != 0) {
+                earth.x = event.motion.x;
+                earth.y = event.motion.y;
+            }
         }
+
+        SDL_FillRect(
+            surface,
+            NULL,
+            SDL_MapRGB(surface->format, 0, 0, 0)
+        );
+
+        
+        FillCircle(
+            surface,
+            (Circle){sun.x, sun.y, sun.radius},
+            COLOR_SUN
+        );
+
+        
+        FillCircle(surface, earth, COLOR_EARTH);
+
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(10);
     }
 
     SDL_DestroyWindow(window);
